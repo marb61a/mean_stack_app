2518039ddea8387c56048cee79c3e87814ebf22c
Martin <marb61a@gmail.com>
2015-03-30 09:00:02 +0000

    Added mongolab database and connected

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "database" : "mongodb://root:root123@ds037581.mongolab.com:37581/myuserstory",
+  "port": process.env.PORT || 3000,
+  "secretKey": "MyNodeSecretKey"
+}
--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-
+var config = require('./config.js');
+var mongoose = require('mongoose');
 var app = express();
 
-app.listen(3000, function(err){
+mongoose.connect(config.database, function(err){
+   if(err){
+    console.log(err);
+  }else{
+    console.log("Connected to the database");
+  }
+  
+});
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('*', function(req, res){
+  res.sendfile(_dirname = '/public/views/index.html');
+});
+
+
+app.listen(config.port, function(err){
   if(err){
     console.log(err);
   }else{
