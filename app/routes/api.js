077eb8d9df37e903d07b4575114529de0383a104
Martin <marb61a@gmail.com>
2015-04-01 08:55:03 +0000

    Added user and login api

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,6 +3,20 @@ var config = require('../../config');
 
 var secretKey = config.secretKey;
 
+var jsonwebtoken = require('jsonwebtoken');
+
+//Must be outside module 
+function createtoken(user){
+  var token = jsonwebtoken.sign({
+    _id: user.id,
+    name: user.username
+  }, superSecret, {
+    expiresInMinutes: 1440
+  });
+  
+  return token;
+}
+
 module.exports = function(app, express){
   var api = express.Router();
   
@@ -20,6 +34,45 @@ module.exports = function(app, express){
       }
       
       res.json({message: 'User has been created'});
+      
+    });
+    
+  });
+  
+  api.get('users', function(req, res){
+    User.find({}, function(err, users){
+      if(err){
+        res.send(err);
+        return;
+      }
+      
+      res.json(users);
+      
+    });
+  });
+  
+  api.post('/login', function(req, res){
+    User.findOne(
+      {
+      username: req.body.username
+      }
+    ).select('password').exec(function(err, user){
+      if(err) throw err;
+      if(!user){
+        res.send({message: "user does not exist"});
+      } else if(user){
+          var validPassword = user.comparedPassword(req.body.password);
+            if(!validPassword){
+              res.send({message: "Invalid Password"});
+            }else{
+              var token = createToken(user);
+              res.json({
+                success: true,
+                message: "Successfully Logged In",
+                token: token
+              })
+            }
+      }
       
     });
     
