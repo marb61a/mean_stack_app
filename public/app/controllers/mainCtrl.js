f2cca1c611b4204717f45f3dcc972811d78b5431
Martin <marb61a@gmail.com>
2015-04-06 08:27:59 +0000

    Added first controller

---
+++ public/app/controllers/mainCtrl.js
@@ -0,0 +1,37 @@
+angular.module('mainCtrl', [])
+
+.controller('MainController', function($rootScope, $location, Auth){
+  var vm = this;
+  
+  vm.loggedIn = Auth.isLogged();
+  $rootScope.$on('$routeChangeStart', function(){
+    vm.loggedIn = Auth.isLoggedIn();
+    Auth.getUser()
+      .then(function(data){
+        var user = data.data;
+    });
+  });
+  
+  vm.doLogin = function(){
+    vm.processing = true;
+    vm.error = '';
+    Auth.login(vm.loginData.username, vm.loginData.password)
+      .success(function(data){
+        vm.processing = false;
+      
+      Auth.getuser()
+        .then(function(data){
+          var user = data.data;
+      });
+      if(data.success)
+         $location.path('/');
+      else
+        var error = data.message;
+    });
+  }
+  
+  vm.doLogout = function(){
+    Auth.logout();
+    $location.path('/logout');
+  }
+});
