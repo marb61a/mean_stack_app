b831b6bffe4321407d57032fce6fbe47acd8c287
Martin <marb61a@gmail.com>
2015-04-07 08:16:31 +0000

    Added login route and page

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -6,4 +6,10 @@ angular.module('appRoutes', ['ngRoute'])
        {
         templateUrl: 'app/views/pages/home.html'
       })
+  .when('/login',
+       {
+        templateUrl: 'app/views/pages/login.html'
+      })
+  
+  $locationProvider.html5mode(true);
 })
