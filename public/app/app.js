b4acef79872a34eab3e4e8257a472eac311b50a5
Martin <marb61a@gmail.com>
2015-04-09 08:49:23 +0000

    added story controller and service

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('MyStoryApp', ['appRoutes', 'mainCtrl','authService', 'userCtrl', 'userService'])
+angular.module('MyStoryApp', ['appRoutes', 'mainCtrl','authService', 'userCtrl', 'userService', 'storyService', 'storyCtrl'])
 
 .config(function($httpProvider){
   $httpProvider.interceptors.push('AuthInterceptor');
--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -4,16 +4,21 @@ angular.module('appRoutes', ['ngRoute'])
   $routeProvider
   .when('/',
        {
-        templateUrl: 'app/views/pages/home.html'
+        templateUrl: 'app/views/pages/home.html',
+        controller: 'MainController',
+        controllerAs: 'main' 
       })
+  
   .when('/login',
        {
         templateUrl: 'app/views/pages/login.html'
       })
+  
   .when('/signup',
        {
         templateUrl: 'app/views/pages/signup.html'
       })
+  
   
   
   $locationProvider.html5mode(true);
